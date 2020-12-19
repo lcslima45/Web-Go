3e541fb8aad2f141a3d261af414780bfa5bfaa5f
lcslima45 <lcslima45@gmail.com>
2020-12-19 16:42:09 -0300

    Passando valores para uma função embutida em um template embutido

--- Exercise-7/main.go
+++ Exercise-7/main.go
@@ -11,6 +11,11 @@ var fm = template.FuncMap{
 	"soma": soma,
 }
 
+type values struct {
+	Value1 int
+	Value2 int
+}
+
 func soma(a, b int) int {
 	return a + b
 
@@ -20,8 +25,11 @@ func init() {
 }
 
 func main() {
-
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", nil)
+	value := values{
+		Value1: 1,
+		Value2: 2,
+	}
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", value)
 	if err != nil {
 		log.Println("Error executing", err)
 	}
